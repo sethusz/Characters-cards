d18f87795dc5c4427b544c892dfcc601f1f7eb85
Oleksandr <oleksandrzelenjak@gmail.com>
2023-02-28 21:32:08 +0200

    deleted unused img and fixed bugs

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-
 import Spinner from '../spinner/spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -10,24 +9,44 @@ class CharList extends Component {
   state = {
     charList: [],
     loading: true,
-    error: false
+    error: false,
+    newItemLoading: false,
+    offset: 210,
+    charEnded: false
   }
 
   marvelService = new MarvelService();
 
   componentDidMount() {
+    this.onRequest();
+  }
 
-    
-    this.marvelService.getAllCharacters()
+  onRequest = (offset) => {
+    this.onCharListLoading();
+    this.marvelService.getAllCharacters(offset)
       .then(this.onCharListLoaded)
       .catch(this.onError)
   }
 
-  onCharListLoaded = (charList) => {
+  onCharListLoading = () => {
     this.setState({
-      charList,
-      loading: false
+      newItemLoading: true
     })
+  }
+
+  onCharListLoaded = (newCharList) => {
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
+    }
+
+    this.setState(({ offset, charList }) => ({
+      charList: [...charList, ...newCharList],
+      loading: false,
+      newItemLoading: false,
+      offset: offset + 9,
+      charEnded: ended
+    }))
   }
 
   onError = () => {
@@ -40,12 +59,16 @@ class CharList extends Component {
   renderItems(arr) {
     const items = arr.map((item) => {
       let imgStyle = { 'objectFit': 'cover' };
-      if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+      if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' || item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708.gif') {
         imgStyle = { 'objectFit': 'unset' };
       }
 
+
       return (
-        <li className="char__item" key={item.id} onClick={(() => this.props.onCharSelected(item.id))}>
+        <li
+          className="char__item"
+          key={item.id}
+          onClick={() => this.props.onCharSelected(item.id)}>
           <img src={item.thumbnail} alt={item.name} style={imgStyle} />
           <div className="char__name">{item.name}</div>
         </li>
@@ -60,7 +83,9 @@ class CharList extends Component {
   }
 
   render() {
-    const { charList, loading, error } = this.state;
+
+    const { charList, loading, error, offset, newItemLoading, charEnded } = this.state;
+
     const items = this.renderItems(charList);
 
     const errorMessage = error ? <ErrorMessage /> : null;
@@ -72,7 +97,11 @@ class CharList extends Component {
         {errorMessage}
         {spinner}
         {content}
-        <button className="button button__main button__long">
+        <button
+          className="button button__main button__long"
+          disabled={newItemLoading}
+          style={{ 'display': charEnded ? 'none' : 'block' }}
+          onClick={() => this.onRequest(offset)}>
           <div className="inner">load more</div>
         </button>
       </div>
@@ -81,5 +110,3 @@ class CharList extends Component {
 }
 
 export default CharList;
-
-
